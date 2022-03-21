538ec07002af1a92b9b854e6630662100c70aefe
korply <87417114+AnodazTeam@users.noreply.github.com>
2022-03-21 20:47:22 +0700

    Swap Player

--- Assets/Script/Controller/Controller.cs
+++ Assets/Script/Controller/Controller.cs
@@ -5,24 +5,62 @@ using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
-    public GameObject humanPrefab;
 
-    void Start()
-    {
-        
-    }
+
+    public HumanMagic MAGhuman;
+    public HumanAgi AGIhuman;
+    public HumanSTR STRhuman;
+    public Slot[] slots1, slots2, slots3;
 
     
+    void Start()
+    {
+        slots1[0].transform.position = transform.position + new Vector3(0, 0, 0);
+        slots2[0].transform.position = transform.position + new Vector3(2, 0, 0);
+        slots3[0].transform.position = transform.position + new Vector3(4, 0, 0);
+        MAGhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+        AGIhuman.transform.position = transform.position + new Vector3(2, 0, 0);
+        STRhuman.transform.position = transform.position + new Vector3(4, 0, 0);
+
+    }
     void Update()
     {
-        
+        swarplocation();
     }
 
     void swarplocation()
     {
-        //swap location
-        // Vector3 temp = transform.position;
-        // transform.position = target.position;
-        // target.position = temp;
+        if (Input.GetKeyDown("q"))
+        {
+            slots1[0].transform.position = transform.position + new Vector3(4, 0, 0);
+            slots2[0].transform.position = transform.position + new Vector3(2, 0, 0);
+            slots3[0].transform.position = transform.position + new Vector3(0, 0, 0);
+            MAGhuman.transform.position = transform.position + new Vector3(4, 0, 0);
+            AGIhuman.transform.position = transform.position + new Vector3(2, 0, 0);
+            STRhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+            Debug.Log("You have Press Q Move Blue To Front");
+        }
+        else if (Input.GetKeyDown("w"))
+        {
+            slots2[0].transform.position = transform.position + new Vector3(4, 0, 0);
+            slots3[0].transform.position = transform.position + new Vector3(2, 0, 0);
+            slots1[0].transform.position = transform.position + new Vector3(0, 0, 0);
+            AGIhuman.transform.position = transform.position + new Vector3(4, 0, 0);
+            STRhuman.transform.position = transform.position + new Vector3(2, 0, 0);
+            MAGhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+            Debug.Log("You have Press W Move Green To Front");
+        }
+        else if(Input.GetKeyDown("e"))
+        {
+            slots3[0].transform.position = transform.position + new Vector3(4, 0, 0);
+            slots1[0].transform.position = transform.position + new Vector3(0, 0, 0);
+            slots2[0].transform.position = transform.position + new Vector3(2, 0, 0);
+            STRhuman.transform.position = transform.position + new Vector3(4, 0, 0);
+            MAGhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+            AGIhuman.transform.position = transform.position + new Vector3(2, 0, 0);
+            Debug.Log("You have Press E Move Red To Front");
+        }
+        {
+        }
     }
 }
