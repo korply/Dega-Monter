ae5f59e566ea4439b3601e7adbd5dff306a78764
TTTT <otfor01@gmail.com>
2022-03-22 17:03:25 +0700

    auto move, double jump
    auto move, double jump

--- Assets/Script/Controller/Controller.cs
+++ Assets/Script/Controller/Controller.cs
@@ -11,56 +11,87 @@ public class Controller : MonoBehaviour
     public HumanAgi AGIhuman;
     public HumanSTR STRhuman;
     public Slot[] slots1, slots2, slots3;
+    public float moveSpeed = 2f;
 
-    
+    private Rigidbody2D rigidbody2d;
+    private CircleCollider2D circleCollider2D;
+    [SerializeField] private LayerMask platformsLayerMask;
+    int jumpCount = 0;
+
     void Start()
     {
-        slots1[0].transform.position = transform.position + new Vector3(0, 0, 0);
-        slots2[0].transform.position = transform.position + new Vector3(2, 0, 0);
-        slots3[0].transform.position = transform.position + new Vector3(4, 0, 0);
-        MAGhuman.transform.position = transform.position + new Vector3(0, 0, 0);
-        AGIhuman.transform.position = transform.position + new Vector3(2, 0, 0);
-        STRhuman.transform.position = transform.position + new Vector3(4, 0, 0);
+        slots1[0].transform.position = transform.position + new Vector3(-1, 0, 0);
+        slots2[0].transform.position = transform.position + new Vector3(0, 0, 0);
+        slots3[0].transform.position = transform.position + new Vector3(1, 0, 0);
+        MAGhuman.transform.position = transform.position + new Vector3(-1, 0, 0);
+        AGIhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+        STRhuman.transform.position = transform.position + new Vector3(1, 0, 0);
 
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        circleCollider2D = transform.GetComponent<CircleCollider2D>();
     }
     void Update()
     {
-        swarplocation();
+        swaplocation();
+        jump();
+        rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
+
     }
 
-    void swarplocation()
+    void swaplocation()
     {
         if (Input.GetKeyDown("q"))
         {
-            slots1[0].transform.position = transform.position + new Vector3(4, 0, 0);
-            slots2[0].transform.position = transform.position + new Vector3(2, 0, 0);
-            slots3[0].transform.position = transform.position + new Vector3(0, 0, 0);
-            MAGhuman.transform.position = transform.position + new Vector3(4, 0, 0);
-            AGIhuman.transform.position = transform.position + new Vector3(2, 0, 0);
-            STRhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+            slots1[0].transform.position = transform.position + new Vector3(1, 0, 0);
+            slots2[0].transform.position = transform.position + new Vector3(0, 0, 0);
+            slots3[0].transform.position = transform.position + new Vector3(-1, 0, 0);
+            MAGhuman.transform.position = transform.position + new Vector3(1, 0, 0);
+            AGIhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+            STRhuman.transform.position = transform.position + new Vector3(-1, 0, 0);
             Debug.Log("You have Press Q Move Blue To Front");
         }
         else if (Input.GetKeyDown("w"))
         {
-            slots2[0].transform.position = transform.position + new Vector3(4, 0, 0);
-            slots3[0].transform.position = transform.position + new Vector3(2, 0, 0);
-            slots1[0].transform.position = transform.position + new Vector3(0, 0, 0);
-            AGIhuman.transform.position = transform.position + new Vector3(4, 0, 0);
-            STRhuman.transform.position = transform.position + new Vector3(2, 0, 0);
-            MAGhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+            slots2[0].transform.position = transform.position + new Vector3(1, 0, 0);
+            slots3[0].transform.position = transform.position + new Vector3(0, 0, 0);
+            slots1[0].transform.position = transform.position + new Vector3(-1, 0, 0);
+            AGIhuman.transform.position = transform.position + new Vector3(1, 0, 0);
+            STRhuman.transform.position = transform.position + new Vector3(0, 0, 0);
+            MAGhuman.transform.position = transform.position + new Vector3(-1, 0, 0);
             Debug.Log("You have Press W Move Green To Front");
         }
         else if(Input.GetKeyDown("e"))
         {
-            slots3[0].transform.position = transform.position + new Vector3(4, 0, 0);
-            slots1[0].transform.position = transform.position + new Vector3(0, 0, 0);
-            slots2[0].transform.position = transform.position + new Vector3(2, 0, 0);
-            STRhuman.transform.position = transform.position + new Vector3(4, 0, 0);
-            MAGhuman.transform.position = transform.position + new Vector3(0, 0, 0);
-            AGIhuman.transform.position = transform.position + new Vector3(2, 0, 0);
+            slots3[0].transform.position = transform.position + new Vector3(1, 0, 0);
+            slots1[0].transform.position = transform.position + new Vector3(-1, 0, 0);
+            slots2[0].transform.position = transform.position + new Vector3(0, 0, 0);
+            STRhuman.transform.position = transform.position + new Vector3(1, 0, 0);
+            MAGhuman.transform.position = transform.position + new Vector3(-1, 0, 0);
+            AGIhuman.transform.position = transform.position + new Vector3(0, 0, 0);
             Debug.Log("You have Press E Move Red To Front");
         }
+        
+       
+    }
+    void jump()
+    {
+       
+        if (IsGrounded())
         {
+            jumpCount = 0;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 1)
+        {
+            float jumpVelocity = 5f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            jumpCount++;
+        }
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(circleCollider2D.bounds.center, circleCollider2D.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
+     
+        return raycastHit2D.collider != null;
     }
 }
